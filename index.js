4e098772e129a56665e78b0884538b149ff1fce9
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-04-05 00:30:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ require('fs').readdirSync(__dirname + '/modules/').forEach(function(file) {
 });
 
 var bot = linebot({
-	channelId: "1635505649",
-  channelSecret: "b59dc842309386b494bb05541e10dfb3",
-  channelAccessToken: "jCV9Fe8nB3+G91MXq2eWCz5v2w+jGsE8A+kVdln0CF3E53aW5nZnzNfnlzkmVgZkf2OAxNahvylD0Z0VJ+wGDrDkfoyMQMvm525qu2T1c3h9FeL8VMMEwbTRXTEtUYO8Bfu5x0xYMwX/aoKJSHYrEAdB04t89/1O/w1cDnyilFU="
+  	channelSecret: process.env.LINE_CHANNEL_SECRET, //這裡是讓系統抓在Heroku設定的數據
+  	channelAccessToken: process.env.LINE_CHANNEL_ACCESSTOKEN // 同上
 });
 
 var app = express();
