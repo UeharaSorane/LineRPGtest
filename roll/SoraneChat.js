155a550c244603ba368b619001654e0caa59ec22
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-04-04 23:12:56 +0800

    Update SoraneChat.js

--- roll/SoraneChat.js
+++ roll/SoraneChat.js
@@ -2,7 +2,7 @@ var rollbase = require('./rollbase.js');
 var rply ={type : 'text'}; //type是必需的,但可以更改
 
 //////////////// 空音閒談
-function randomReply(userName) {
+function randomReply(userName,chat) {
 	let rplyArr = [
 
 		userName+'，感覺你很閒呢…能一直找我聊天...',
@@ -25,6 +25,7 @@ function randomReply(userName) {
 		'\我的父親(?)，施彥任內心有點脆弱，拜托沒事不要傷他的心喔。',
 		'\比起一直找我聊天，不如試著找其他事做吧。',
 		'\稍微...讓我休息一下吧(攤'];
+	system.log(chat);
 	rply.text =  rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 
 		return ['rply',rply];
