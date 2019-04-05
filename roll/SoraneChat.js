e80fd490e8924e3ea2e7aa7085759356cdf06aec
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-04-05 13:48:40 +0800

    Update SoraneChat.js

--- roll/SoraneChat.js
+++ roll/SoraneChat.js
@@ -18,45 +18,63 @@ function randomReply(userID,userName,chat) {
 	var rplyArr = [] ;
 	WitClient.message(chat, {}).then((data) => {
 		console.log(data.entities);
-		if(data.entities.thinking != null){
-			rplyArr = ['\痾...不予置評。',
-				   '你何不問問神奇海螺呢？',
-				  '\嗯....(專心打太鼓達人中)'];
-		}else if(data.entities.sorry != null){
-			rplyArr = ['你想要我道歉嗎？但是我內心毫無悔意。',
-				   '對...不...起...嘛...QAQ'
-				  ];
-		}else if(data.entities.starburst != null){
+		
+		if(data.entities.starburst != null){
 			rplyArr = [userName+'\！幫我撐十秒！！！', 
 				   '化成'
 				  ];
-		}else if(data.entities.wtf != null){
-			rplyArr = [userName+'\藍色那包記得去吃', 
-				   '蛤？',
-				   '鬧爆',
-				   '比起做這些小動作，你為何不先糾正自己的品格呢？'
-				  ];
-		}else if(data.entities.calling != null){
-			rplyArr = [
-				userName+'，感覺你很閒呢…能一直找我聊天...',
-				'\有什麼事嗎？', 
-				'\如果沒有需要，我回去打街機喔。',  
-				'\如果有不會的指令的話，輸入help吧！',
-				'\Wryyyyyyyyyyyyyyyyyy!!!!!(DIO臉)。',
-				'\攤',
-				'\儘管我只是一個bot，也是有遊戲要玩的。',
-				'\想要確認自己的手氣的話，試試輸入"運氣"如何？',
-				'\所以我說'+userName+'啊！你應該也有要做的事吧？',
-				'\話說'+userName+'，你也喜歡音G嗎？',
-				'\我是擔當bot而不是教機率學的，如果妳臉黑，我只會叫你儲值(被巴。',
-				'\！',
-				'\接下我最後的波紋吧！'+userName+'!!!!!',
-				'\我的喜好嗎...？當然是音樂遊戲啊！',
-				'\我的父親(?)，施彥任內心有點脆弱，拜托沒事不要傷他的心喔。',
-				'\比起一直找我聊天，不如試著找其他事做吧。',
-				'\稍微...讓我休息一下吧(攤'
-			];
+		}else{
+			if(data.entities.thinking != null){
+				if(data.entites.foodshop.length > 1){
+					let shopchoice = [];
+					for(var i = 0;i<data.entites.foodshop.length;i++){
+						shopchoice[i] = data.entites.foodshop[i].value;
+					}
+					rplyArr = [
+						'你可以選擇' + shopchoice[Math.floor((Math.random() * (shopchoice.length)) + 0)],
+						'你可以試試' + shopchoice[Math.floor((Math.random() * (shopchoice.length)) + 0)],\
+						'我覺得你該追求自己的本性',
+						'你來決定吧'
+					];
+					
+				}else{
+					rplyArr = ['\痾...不予置評。',
+						   '你何不問問神奇海螺呢？',
+						  '\嗯....(專心打太鼓達人中)'];
+				}
+			}else if(data.entities.sorry != null){
+				rplyArr = ['你想要我道歉嗎？但是我內心毫無悔意。',
+					   '對...不...起...嘛...QAQ'
+					  ];
+			}else if(data.entities.wtf != null){
+				rplyArr = [userName+'\藍色那包記得去吃', 
+					   '蛤？',
+					   '鬧爆',
+					   '比起做這些小動作，你為何不先糾正自己的品格呢？'
+					  ];
+			}else if(data.entities.calling != null){
+				rplyArr = [
+					userName+'，感覺你很閒呢…能一直找我聊天...',
+					'\有什麼事嗎？', 
+					'\如果沒有需要，我回去打街機喔。',  
+					'\如果有不會的指令的話，輸入help吧！',
+					'\Wryyyyyyyyyyyyyyyyyy!!!!!(DIO臉)。',
+					'\攤',
+					'\儘管我只是一個bot，也是有遊戲要玩的。',
+					'\想要確認自己的手氣的話，試試輸入"運氣"如何？',
+					'\所以我說'+userName+'啊！你應該也有要做的事吧？',
+					'\話說'+userName+'，你也喜歡音G嗎？',
+					'\我是擔當bot而不是教機率學的，如果妳臉黑，我只會叫你儲值(被巴。',
+					'\！',
+					'\接下我最後的波紋吧！'+userName+'!!!!!',
+					'\我的喜好嗎...？當然是音樂遊戲啊！',
+					'\我的父親(?)，施彥任內心有點脆弱，拜托沒事不要傷他的心喔。',
+					'\比起一直找我聊天，不如試著找其他事做吧。',
+					'\稍微...讓我休息一下吧(攤'
+				];
+			}
 		}
+		
 		rply.text =  rplyArr[Math.floor((Math.random() * (rplyArr.length)) + 0)];
 		bot.push(userID,rply.text);
 		//bot.push("U7b7830437667bf4b7b54eaf02e762690",data.entities);
