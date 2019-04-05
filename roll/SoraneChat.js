d4c252bafed15f377d6475120c708ca463a17c34
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-04-05 11:27:14 +0800

    Update SoraneChat.js

--- roll/SoraneChat.js
+++ roll/SoraneChat.js
@@ -17,7 +17,7 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
 function randomReply(userID,userName,chat) {
 	var rplyArr = [] ;
 	WitClient.message(chat, {}).then((data) => {	
-		if(entities == "thinking"){
+		if(data.entities == "thinking"){
 			rplyArr = ['你何不問問神奇海螺呢？'];
 		}else{
 			rplyArr = [
